eeebb259e40e079f7626dd506e9e1b3249abd4d0
dodadadaaan <56538733+dodadadaaan@users.noreply.github.com>
2024-04-10 19:07:59 +0900

    add

---
+++ hw1/tsuchida/answer1.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    a = 2
+    b = 3
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
---
+++ hw1/tsuchida/answer2-1.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    a = ["apple",10,False]
+    b = "orange"
+
+    a.append(b)
+    a[1] = 5
+
+    print(a)
