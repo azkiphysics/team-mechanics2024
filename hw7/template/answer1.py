fb78a99447e37014eea09bc2545746eae7685c70
azkiphysics <56015389+azkiphysics@users.noreply.github.com>
2024-04-22 20:33:26 +0900

    update

---
+++ hw7/template/answer1.py
@@ -0,0 +1,229 @@
+import argparse
+import logging
+from logging import getLogger, Formatter, StreamHandler
+from typing import Dict, List
+
+import matplotlib.pyplot as plt
+import numpy as np
+import yaml
+from tqdm import trange
+from tqdm.contrib.logging import logging_redirect_tqdm
+
+from common.agents import Agent, LQRAgent
+from common.buffers import Buffer
+from common.envs import Env, CartPoleEnv
+from common.utils import FigureMaker, MovieMaker
+from common.wrappers import LQRMultiBodyEnvWrapper
+
+# ロガーの設定
+logger = getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler_format = Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler = StreamHandler()
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(handler_format)
+logger.addHandler(handler)
+
+# Matplotlibで綺麗な論文用のグラフを作る
+# https://qiita.com/MENDY/items/fe9b0c50383d8b2fd919
+plt.rcParams["font.family"] = "Times New Roman"  # font familyの設定
+plt.rcParams["mathtext.fontset"] = "stix"  # math fontの設定
+plt.rcParams["font.size"] = 15  # 全体のフォントサイズが変更されます。
+plt.rcParams["xtick.labelsize"] = 15  # 軸だけ変更されます。
+plt.rcParams["ytick.labelsize"] = 15  # 軸だけ変更されます
+plt.rcParams["xtick.direction"] = "in"  # x axis in
+plt.rcParams["ytick.direction"] = "in"  # y axis in
+plt.rcParams["axes.linewidth"] = 1.0  # axis line width
+plt.rcParams["axes.grid"] = True  # make grid
+plt.rcParams["axes.axisbelow"] = True  # グリッドを最背面に移動
+
+
+class Runner(object):
+    def __init__(
+        self,
+        env_config: Dict[str, Env | Dict[str, int | float] | List[Dict[str, int | float]]],
+        agent_config: Dict[str, Agent | Dict[str, int | float]],
+        buffer_config: Dict[str, Buffer | Dict[str, int]],
+    ) -> None:
+        self.env_config = env_config
+        self.agent_config = agent_config
+        self.buffer_config = buffer_config
+
+        self.env: Env = env_config["class"](**env_config["init"])
+        if "wrappers" in env_config:
+            for env_wrapper_config in self.env_config["wrappers"]:
+                self.env = env_wrapper_config["class"](self.env, **env_wrapper_config["init"])
+        self.agent: Agent = agent_config["class"](self.env, **agent_config["init"])
+        self.buffer: Buffer = buffer_config["class"](**buffer_config["init"])
+        self.run_result = Buffer()
+        self.evaluate_result = Buffer()
+        self.figure_maker = FigureMaker()
+        self.movie_maker = MovieMaker()
+
+    def reset(self):
+        self.env.reset(**self.env_config["reset"])
+        self.agent.reset(**self.agent_config["reset"])
+        self.buffer.reset(**self.buffer_config["reset"])
+        self.run_result.reset()
+        self.evaluate_result.reset()
+        self.figure_maker.reset()
+        self.movie_maker.reset()
+
+    def run(self, total_timesteps: int, learning_starts: int = 1000, trainfreq: int | None = None) -> Dict[str, float]:
+        k_episodes = 0
+        total_rewards = 0.0
+        obs, _ = self.env.reset(**self.env_config["reset"])
+        with logging_redirect_tqdm(loggers=[logger]):
+            for k_timesteps in trange(total_timesteps, leave=False):
+                action = self.agent.act(obs)
+                next_obs, reward, terminated, truncated, _ = self.env.step(action)
+                done = terminated or truncated
+                self.buffer.push(
+                    {
+                        "obs": obs.copy(),
+                        "next_obs": next_obs.copy(),
+                        "action": action.copy(),
+                        "reward": reward,
+                        "done": done,
+                    }
+                )
+                total_rewards += reward
+                if (
+                    k_timesteps >= learning_starts
+                    and trainfreq is not None
+                    and trainfreq > 0
+                    and k_timesteps % trainfreq == 0
+                ):
+                    self.agent.train(buffer=self.buffer)
+                if done:
+                    logger.info(f"episode: {k_episodes}/ total_rewards: {total_rewards}")
+                    k_episodes += 1
+                    total_rewards = 0.0
+                    obs, _ = self.env.reset(**self.env_config["reset"])
+                obs = next_obs.copy()
+                self.run_result.push({"episode": k_episodes, "total_rewards": total_rewards})
+
+    def evaluate(self, moviefreq: int = 100):
+        # シミュレーションの実行
+        obs, info = self.env.reset(**self.env_config["reset"])
+        self.agent.reset(**self.agent_config["reset"], is_evaluate=True)
+        done = False
+        self.evaluate_result.reset()
+        self.evaluate_result.push(info)
+        self.movie_maker.reset()
+        self.movie_maker.add(self.env.render())
+        k_steps = 0
+        while not done:
+            k_steps += 1
+            action = self.agent.act(obs)
+            next_obs, _, terminated, truncated, info = self.env.step(action)
+            self.evaluate_result.push(info)
+            done = terminated or truncated
+            if k_steps % moviefreq == 0 or done:
+                self.movie_maker.add(self.env.render())
+            if done:
+                break
+            obs = next_obs.copy()
+
+    def save(self, savedir: str):
+        if len(self.run_result) > 0:
+            # 学習結果の保存
+            training_data = self.run_result.get()
+            total_rewards_data = {
+                "x": {"label": "Episode", "value": np.array(training_data["episode"], dtype=np.float64)},
+                "y": {
+                    "label": "State $s$",
+                    "value": np.array(training_data["total_rewards"], dtype=np.float64),
+                },
+            }
+            self.figure_maker.reset()
+            self.figure_maker.make(total_rewards_data)
+            savefile = "total_rewards.png"
+            self.figure_maker.save(savedir, savefile)
+
+        if len(self.evaluate_result) > 0:
+            evaluate_data = self.evaluate_result.get()
+            # 状態の保存
+            state_data = {
+                "x": {"label": "Time $t$ s", "value": np.array(evaluate_data["t"], dtype=np.float64)},
+                "y": {
+                    "label": "State $s$",
+                    "value": [
+                        {"label": "$" + f"s_{idx + 1}" + "$", "value": state_idx}
+                        for idx, state_idx in enumerate(np.array(evaluate_data["s"], dtype=np.float64).T)
+                    ],
+                },
+            }
+            savefile = "state.png"
+            self.figure_maker.reset()
+            self.figure_maker.make(state_data)
+            self.figure_maker.save(savedir, savefile=savefile)
+
+            # 制御入力の保存
+            u_data = {
+                "x": {"label": "Time $t$ s", "value": np.array(evaluate_data["t"], dtype=np.float64)[:-1]},
+                "y": {
+                    "label": "Control input $u$",
+                    "value": [
+                        {"label": "$" + f"u_{idx + 1}" + "$", "value": u_idx}
+                        for idx, u_idx in enumerate(np.array(evaluate_data["u"], dtype=np.float64).T)
+                    ],
+                },
+            }
+            savefile = "u.png"
+            self.figure_maker.reset()
+            self.figure_maker.make(u_data)
+            self.figure_maker.save(savedir, savefile=savefile)
+
+            # 動画の保存
+            savefile = "animation.mp4"
+            self.movie_maker.make(savedir, evaluate_data["t"][-1], savefile=savefile)
+
+
+ENVS = {"CartPoleEnv": CartPoleEnv}
+AGENTS = {"LQR": LQRAgent}
+BUFFERS = {"Buffer": Buffer}
+RUNNERS = {"Runner": Runner}
+WRAPPERS = {"LQRMultiBody": LQRMultiBodyEnvWrapper}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Gymnasium training")
+    parser.add_argument("config_path", type=str)
+    args = parser.parse_args()
+
+    # configの読み込み
+    with open(args.config_path) as f:
+        config = yaml.safe_load(f)
+
+    # 環境の設定
+    env_config = {
+        "class": ENVS[config["env"]["name"]],
+        "wrappers": [
+            {"class": WRAPPERS[wrapper["name"]], "init": wrapper["init"]} for wrapper in config["env"]["wrappers"]
+        ],
+        "init": config["env"]["init"],
+        "reset": config["env"]["reset"],
+    }
+
+    # エージェントの設定
+    agent_config = {
+        "class": AGENTS[config["agent"]["name"]],
+        "init": config["agent"]["init"],
+        "reset": config["agent"]["reset"],
+    }
+
+    # バッファの設定
+    buffer_config = {
+        "class": BUFFERS[config["buffer"]["name"]],
+        "init": config["buffer"]["init"],
+        "reset": config["buffer"]["reset"],
+    }
+
+    # Runnerの設定
+    runner: Runner = RUNNERS[config["runner"]["name"]](
+        env_config, agent_config, buffer_config, **config["runner"]["init"]
+    )
+    runner.reset(**config["runner"]["reset"])
+    runner.run(**config["runner"]["run"])
+    runner.evaluate(**config["runner"]["evaluate"])
+    runner.save(**config["runner"]["save"])
---
+++ hw7/template/common/wrappers.py
@@ -0,0 +1,207 @@
+from typing import Dict, List, Tuple
+import numpy as np
+
+from .envs import Env, MultiBodyEnv
+from .utils import Box
+
+
+class Wrapper(object):
+    def __init__(self, env: Env) -> None:
+        self.env = env
+
+    @property
+    def x_space(self) -> Box:
+        return self.env.x_space
+
+    @property
+    def u_space(self) -> Box:
+        return self.env.u_space
+
+    @property
+    def state_space(self) -> Box:
+        return self.env.state_space
+
+    @property
+    def observation_space(self) -> Box:
+        return self.env.observation_space
+
+    @property
+    def action_space(self) -> Box:
+        return self.env.action_space
+
+    def get_state(self, x: np.ndarray) -> np.ndarray:
+        return self.env.get_state(x)
+
+    def get_observation(self, t: float, x: np.ndarray, u: np.ndarray | None = None) -> np.ndarray:
+        return self.get_state(x)
+
+    def convert_action(self, action: np.ndarray) -> np.ndarray:
+        return action
+
+    def get_reward(self, t: float, x: np.ndarray, u: np.ndarray) -> float:
+        return self.env.get_reward(t, x, u)
+
+    def get_terminated(self, t: float, x: np.ndarray, u: np.ndarray) -> bool:
+        return self.env.get_terminated(t, x, u)
+
+    def get_truncated(self, t: float, x: np.ndarray, u: np.ndarray) -> bool:
+        return self.env.get_truncated(t, x, u)
+
+    def reset(
+        self, initial_t: float, initial_x: np.ndarray, integral_method: str = "runge_kutta_method", **kwargs
+    ) -> Tuple[np.ndarray, Dict[str, bool | float | np.ndarray]]:
+        _, info = self.env.reset(initial_t, initial_x, integral_method=integral_method, **kwargs)
+        t = info.get("t")
+        x = info.get("x")
+        state = self.get_state(x)
+        obs = self.get_observation(t, x)
+        info |= {"s": state.copy()}
+        return obs, info
+
+    def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool, bool, Dict[str, bool | float | np.ndarray]]:
+        action = self.convert_action(action)
+        _, _, _, _, info = self.env.step(action)
+        t = info.get("t")
+        x = info.get("x")
+        u = info.get("u")
+        next_state = self.get_state(x)
+        next_obs = self.get_observation(t, x, u)
+        reward = self.get_reward(t, x, u)
+        terminated = self.get_terminated(t, x, u)
+        truncated = self.get_truncated(t, x, u)
+        info |= {"s": next_state.copy()}
+        return next_obs, reward, terminated, truncated, info
+
+    def render(self) -> np.ndarray:
+        return self.env.render()
+
+    @property
+    def unwrapped(self) -> Env:
+        return self.env.unwrapped
+
+
+class MultiBodyEnvWrapper(Wrapper):
+    def __init__(self, env: MultiBodyEnv) -> None:
+        self.env = env
+
+        self.target_x: np.ndarray = None
+
+    def get_state(self, x: np.ndarray) -> np.ndarray:
+        state_indices = self.env.unwrapped.get_state_indices()
+        state = x[state_indices].astype(self.state_space.dtype)
+        target_state = self.target_x[state_indices].astype(self.state_space.dtype)
+        return state - target_state
+
+    def reset(
+        self,
+        initial_t: float,
+        initial_x: np.ndarray,
+        target_x: List[float] | np.ndarray,
+        Q: float | np.ndarray,
+        R: float | np.ndarray,
+        integral_method: str = "runge_kutta_method",
+        **kwargs,
+    ) -> Tuple[np.ndarray | Dict[str, bool | float | np.ndarray]]:
+        n_obs = self.observation_space.shape[0]
+        n_us = self.u_space.shape[0]
+        self.Q = Q * np.identity(n_obs, dtype=np.float64)
+        self.R = R * np.identity(n_us, dtype=np.float64)
+        initial_x = np.array(initial_x, dtype=np.float64)
+        target_x = np.array(target_x, dtype=np.float64)
+        self.target_x = self.env.unwrapped.newton_raphson_method(initial_t, target_x)
+        return super().reset(initial_t=initial_t, initial_x=initial_x, integral_method=integral_method, **kwargs)
+
+    @property
+    def unwrapped(self) -> MultiBodyEnv:
+        return self.env.unwrapped
+
+
+class LQRMultiBodyEnvWrapper(MultiBodyEnvWrapper):
+    def __init__(self, env: MultiBodyEnv) -> None:
+        super().__init__(env)
+
+        self.Q: np.ndarray = None
+        self.R: np.ndarray = None
+
+    def compute_ddqi(self, t: float, x: np.ndarray, u: np.ndarray):
+        pos_indices = self.env.unwrapped.get_coordinate_indices()
+        independent_pos_indices = self.env.unwrapped.get_independent_coordinate_indices()
+        dependent_pos_indices = self.env.unwrapped.get_dependent_coordinate_indices()
+
+        # Biの計算
+        Cq = self.env.unwrapped.compute_Cq(t, x)
+        Cqi = Cq[:, independent_pos_indices]
+        Cqd = Cq[:, dependent_pos_indices]
+        Cdi = -np.linalg.inv(Cqd) @ Cqi
+        Bi = np.zeros((len(pos_indices), len(independent_pos_indices)), dtype=np.float64)
+        Bi[independent_pos_indices] = np.identity(len(independent_pos_indices))
+        Bi[dependent_pos_indices] = Cdi
+
+        # ddqの計算
+        M = self.env.unwrapped.compute_mass_matrix(t, x)[: len(pos_indices), : len(pos_indices)]
+        Q = self.env.unwrapped.compute_external_force(t, x, u)
+        Qe = Q[: len(pos_indices)]
+        Qd = Q[len(pos_indices) :]
+        Cd = np.linalg.inv(Cqd) @ Qd
+        gammai = np.zeros(len(pos_indices), dtype=np.float64)
+        gammai[dependent_pos_indices] = Cd
+        return np.linalg.inv(Bi.T @ M @ Bi) @ (Bi.T @ Qe - Bi.T @ M @ gammai)
+
+    @property
+    def A(self) -> np.ndarray:
+        assert self.env.unwrapped.initial_t is not None and self.target_x is not None
+        state_indices = self.env.unwrapped.get_state_indices()
+        n_states = len(state_indices)
+        A = np.zeros((n_states, n_states), dtype=np.float64)
+        A[: n_states // 2, n_states // 2 :] = np.identity(n_states // 2)
+        u = np.zeros(self.env.unwrapped.u_space.shape[0], dtype=self.env.unwrapped.u_space.dtype)
+        h = 1e-7
+        for idx in range(n_states):
+            target_x_plus = self.target_x.copy()
+            target_x_plus[state_indices[idx]] += h
+            ddqi_plus = self.compute_ddqi(self.env.unwrapped.initial_t, target_x_plus, u)
+
+            target_x_minus = self.target_x.copy()
+            target_x_minus[state_indices[idx]] -= h
+            ddqi_minus = self.compute_ddqi(self.env.unwrapped.initial_t, target_x_minus, u)
+
+            A[n_states // 2 :, idx] = (ddqi_plus - ddqi_minus) / (2 * h)
+        return A
+
+    @property
+    def B(self) -> np.ndarray:
+        assert self.env.unwrapped.initial_t is not None and self.target_x is not None
+        state_indices = self.env.unwrapped.get_state_indices()
+        n_states = len(state_indices)
+        n_us = self.env.unwrapped.u_space.shape[0]
+        B = np.zeros((n_states, n_us), dtype=np.float64)
+        u = np.zeros(n_us, dtype=self.env.unwrapped.u_space.dtype)
+        h = 1e-7
+        for idx in range(n_us):
+            u_plus = u.copy()
+            u_plus[idx] += h
+            ddqi_plus = self.compute_ddqi(self.env.unwrapped.initial_t, self.target_x, u_plus)
+
+            u_minus = u.copy()
+            u_minus[idx] -= h
+            ddqi_minus = self.compute_ddqi(self.env.unwrapped.initial_t, self.target_x, u_minus)
+
+            B[n_states // 2 :, idx] = (ddqi_plus - ddqi_minus) / (2 * h)
+        return B
+
+    @property
+    def C(self) -> np.ndarray:
+        n_states = self.state_space.shape[0]
+        n_obs = self.observation_space.shape[0]
+        return np.eye(n_obs, n_states, dtype=np.float64)
+
+    @property
+    def D(self) -> np.ndarray:
+        n_obs = self.observation_space.shape[0]
+        n_us = self.env.unwrapped.u_space.shape[0]
+        return np.zeros((n_obs, n_us), dtype=np.float64)
+
+    def get_reward(self, t: float, x: np.ndarray, u: np.ndarray) -> float:
+        s = self.get_state(x)
+        cost = 0.5 * (s @ self.Q @ s + u @ self.R @ u) * self.env.unwrapped.dt
+        return -cost
