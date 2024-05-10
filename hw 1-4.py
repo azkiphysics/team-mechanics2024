926f4e293dcf92e2c71ed7787fea5276ae221f24
takamatsushuto <168102817+takamatsushuto@users.noreply.github.com>
2024-05-10 12:10:15 +0900

    hw1-1
    課題1を提出します

---
+++ hw 1-4.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    a = {}
+
+    a["fruit"] = "apple"
+
+    print(a)
---
+++ hw 1-5.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    a = 1
+    b = 2
+
+    print("a = b ...", a == b)
+    print("a != b ...", a != b)
