8fbf4b52f2dd0eaacf43763abdd74fc10d400181
dodadadaaan <56538733+dodadadaaan@users.noreply.github.com>
2024-05-16 15:21:46 +0900

    add

---
+++ hw2/tsuchida/answer2-2.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    ans = 0
+    for i in range(10):
+        if i % 2 == 0:
+            ans += i
+
+    print(ans)
---
+++ hw2/tsuchida/anwer3.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    n = 0
+    while 2**n <= 1000:
+        n += 1
+
+    print(n)
